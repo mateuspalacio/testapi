084cf7a601fd2db7e9e1fa003e897d72e62d8c8e
Mateus Palácio <mateuspalacio@gmail.com>
2022-02-18 20:42:03 -0300

    first

---
+++ Api/Controllers/AncientController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AncientController : ControllerBase
+    {
+        private static readonly string[] Ancients = new[]
+         {
+        "Venat", "Hades", "Hythlodaeus", "Hermes", "Themis", "Erichthonios", "Hesperos", "Meteion", "Athena", "Phoinix"
+    };
+
+        private readonly ILogger<AncientController> _logger;
+
+        public AncientController(ILogger<AncientController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetAncient")]
+        public Ancient Get()
+        {
+            return new Ancient()
+            {
+                Name = Ancients[Random.Shared.Next(Ancients.Length)]
+            };
+        }
+    }
+}
